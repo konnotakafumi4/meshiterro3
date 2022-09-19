acd6288ba69f315244e91eb3bbcc5ef1dd03d850
konnotakafumi4 <longwenjinye@gmail.com>
2022-09-19 12:44:31 +0000

    [Finish]4-12

--- app/controllers/post_images_controller.rb
+++ app/controllers/post_images_controller.rb
@@ -14,6 +14,10 @@ class PostImagesController < ApplicationController
     @post_images = PostImage.all
   end
 
+  def show
+    @post_image = PostImage.find(params[:id])
+  end
+
   private
   def post_image_params
     params.require(:post_image).permit(:shop_name, :image, :caption)
